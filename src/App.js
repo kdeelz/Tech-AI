c51be2a5137c12fa514ad3becbbcd11d61fc1422
kdeelz <it21801822@my.sliit.lk>
2024-08-15 19:12:03 +0530

    First with Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CustomNavbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <CustomNavbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* Add other routes here */}
+        </Routes>
+      </Router>
+    </>
   );
+}
+
+function Home() {
+  return <h1>Home Page</h1>;
 }
 
 export default App;
